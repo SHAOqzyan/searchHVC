6b7ed35e119fa64d378e49858dcb37652c725d52
yqz206@gmail.com <yqz206@gmail.com>
2020-09-23 22:04:01 +0800

    bacl

--- searchHVC.py
+++ searchHVC.py
@@ -253,14 +253,7 @@ class seachhvc(object):
         rc('text', usetex=True)
         rc('font', **{'family': 'sans-serif', 'size': 18, 'serif': ['Helvetica']})
 
-        mpl.rcParams['text.latex.preamble'] = [
-            r'\usepackage{tgheros}',  # helvetica font
-            r'\usepackage{sansmath}',  # math-font matching  helvetica
-            r'\sansmath'  # actually tell tex to use it!
-            r'\usepackage{siunitx}',  # micro symbols
-            r'\sisetup{detect-all}',  # force siunitx to use the fonts
-        ]
-
+ 
 
         for eachRow in TB:
 
