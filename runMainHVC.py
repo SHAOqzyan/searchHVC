e8bdfbae0b11327f3626e490b058d3867832128b
yqz206@gmail.com <yqz206@gmail.com>
2020-09-24 08:38:10 +0800

    aaa

--- runMainHVC.py
+++ runMainHVC.py
@@ -16,7 +16,7 @@ doHVC=seachhvc()
 
 dataPath="/share/data/mwisp/G130+00/"
 
-if 1:
+if 0:
 
     allCOFITS=doHVC.getAllCO12FITS(dataPath)
 
@@ -24,12 +24,12 @@ if 1:
         print eachCO
         doHVC.searchCloud(eachCO,outPath=doHVC.tmpPath )
 
-if 1:
+if 0:
     comBinTB=doHVC.getAllTBFiles(doHVC.tmpPath )
 
     #print len( comBinTB  )
 
-if 1:
+if 0:
     doHVC.checkTB("combinedTB.fit")
 
 if 1:
@@ -37,10 +37,10 @@ if 1:
     TB=Table.read("combinedTB.fit")
 
 
-    part1TB= doFITS.selectTBByColRange(TB,"v_cen", minV=90 )
+    part1TB= doFITS.selectTBByColRange(TB,"v_cen", minV=30 )
 
     doHVC.drawCloudSpectra(part1TB ,doHVC.tmpPath )
 
 
-    part2TB= doFITS.selectTBByColRange(TB,"v_cen", maxV=-30 )
+    part2TB= doFITS.selectTBByColRange(TB,"v_cen", maxV=-90 )
     doHVC.drawCloudSpectra(part2TB ,doHVC.tmpPath)
